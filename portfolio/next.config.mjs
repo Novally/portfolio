38c096dcc570d174c69ba1b354e302d69c90e0f4
Ally <54911412+Novally@users.noreply.github.com>
2025-02-07 02:20:28 +0000

    Added next.js files

---
+++ portfolio/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+module.exports = {
+    basePath: '/your-repo-name',  // Replace with your actual repo name
+    assetPrefix: '/your-repo-name/',  // Same here
+  }
+
+  
