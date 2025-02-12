f01fd4bfda99a2bb26a5e151d87f979704b9fd7f
Ally <54911412+Novally@users.noreply.github.com>
2025-02-12 03:06:21 +0000

    pages working-ish

--- portfolio/src/app/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import {AppProps} from 'next/app';
-import React from 'react';
-
-function App({Component, pageProps}: AppProps) {
-  if (typeof window === 'undefined') return null;
-  return <Component {...pageProps} />;
-}
-export default App;
